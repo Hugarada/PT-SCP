84f65f1bd6b2e61b32d5e79752fbfe4b3c3a46f8
Rodolfo Filipe Coelho Belchior <119739@Cooptecnica.local>
2021-04-26 15:51:56 +0100

    Rodolfo's commit

---
+++ Form2.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
+//Getting the SCP html code
+
+
+namespace SCP
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        int actualmenu;
+        Random rnd = new Random();
+
+        // Loading menu
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            Usernamecont.Text = impclass.username;
+            label1.Text = ("Welcome Back " + impclass.username);
+            if (impclass.admin == false)
+            {
+                button7.Visible = false;
+            }
+            else
+            {
+                button7.Visible = true;
+            }
+        }
+
+        //Menu button
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+        //Articles button
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+
+        //Write an article
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+        //Profile button
+        private void button4_MouseEnter(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+        //Log Out button
+        private void button5_MouseEnter(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+        //Exit button
+        private void button6_MouseEnter(object sender, EventArgs e)
+        {
+            button6.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+        // Manage profiles
+        private void button7_MouseEnter(object sender, EventArgs e)
+        {
+            button7.BackColor = Color.FromArgb(5, 103, 241);
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Black;
+            button2.ForeColor = Color.White;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Black;
+            button2.ForeColor = Color.White;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            button3.BackColor = Color.Black;
+            button3.ForeColor = Color.White;
+        }
+
+        private void button4_MouseLeave(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.Black;
+            button4.ForeColor = Color.White;
+        }
+
+        private void button5_MouseLeave(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.Black;
+            button5.ForeColor = Color.White;
+        }
+
+        private void button6_MouseLeave(object sender, EventArgs e)
+        {
+            button6.BackColor = Color.Black;
+            button6.ForeColor = Color.White;
+        }
+
+        private void button7_MouseLeave(object sender, EventArgs e)
+        {
+            button7.BackColor = Color.Black;
+            button7.ForeColor = Color.White;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.BackColor = Color.White;
+            button5.ForeColor = Color.Black;
+            this.Refresh();
+            try
+            {
+                Login li = new Login();
+                li.Show();
+                this.Hide();
+            }
+            catch (Exception ee)
+            {
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.BackColor = Color.White;
+            button6.ForeColor = Color.Black;
+            this.Refresh();
+            Application.Exit();
+        }
+
+        private void panelcleanner ()
+        {
+            panelmenu.Visible = false;
+            //Articles.Visible = false;
+            //WaA.Visible = false;
+            //Profile.Visible = false;
+            //Mprofiles.Visible = false;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelcleanner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panelcleanner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panelcleanner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panelcleanner();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            panelcleanner();
+        }
+    }
+}
---
+++ impclass.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Security.Cryptography;
+using BusinessLogicLayer;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+using BusinessLogicLayer;
+using System.Data;
+
+namespace SCP
+{
+    class impclass
+    {
+        public static string username;
+        public static bool admin;
+        msgdisplayer msg = new msgdisplayer();
+
+        public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public void logging(string email, string pass)
+        {
+            pass = ComputeSha256Hash(pass);
+            try
+            {
+                DataTable dt = new DataTable();
+                dt = BLL.Employees.queryEmployee2(email, pass);
+                if (dt.Rows.Count == 1)
+                {
+                    string Nome = dt.Rows[0]["Name"].ToString();
+                    username = Nome;
+                    Login lg = new Login();
+                    lg.Show();
+                }
+                else
+                {
+                    msg.msg_giver("Conta tem as credencias erradas ou não existe...");
+                    msg.Show();
+                }
+            }
+            catch(Exception ee)
+            {
+                msg.msg_giver("Ocorreu um erro: " + ee.Message);
+                msg.Show();
+            }
+        }
+
+        public void Registering(string Nome, string e, string pass)
+        {
+            pass = ComputeSha256Hash(pass);
+            try
+            {
+                DataTable dt = new DataTable();
+                dt = BLL.Employees.queryEmployee(e);
+                if (dt.Rows.Count == 0)
+                {
+                    BLL.Employees.insertEmployee(Nome, e, pass);
+                    MessageBox.Show("A sua conta foi criada com sucesso");
+                    Login ls = new Login();
+                    ls.Show();
+                    Register rh = new Register();
+                    rh.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username ou Email, já existem...");
+                }
+            }
+            catch(Exception ee)
+            {
+                msg.msg_giver(ee.Message);
+                msg.Show();
+            }
+        }
+
+        public void tec()
+        {
+            Process process = Process.Start(new ProcessStartInfo(
+            ((Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\osk.exe"))));
+        }
+    }
+}
